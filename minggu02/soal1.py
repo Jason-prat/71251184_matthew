cb6396a6f75f1c099edcab3b8fd0a9e871875585
Jason-prat <matthew.jason@ti.ukdw.ac.id>
2026-02-12 22:01:59 +0700

    .

--- minggu02/soal1.py
+++ minggu02/soal1.py
@@ -1,6 +1,6 @@
 print("List BMI:\nKekurangan BB: < 18.5\nNormal: 18.5-24.9\nKelebihan BB: 25-29.9\nObesitas: 30 <")
-tinggi = float(input("Masukkan Tinggi Badan dalam Meter: "))
+tinggi = float(input("Masukkan Tinggi Badan (Cm): "))
 BMI = float(input("Masukkan BMI yang ingin dicapai: "))
 
-BeratBadan = BMI * tinggi**2
-print(f"Berat Badan yang ideal untuk mencapai BMI yang diinginkan adalah: {BeratBadan}Kg")
+BeratBadan = BMI * (tinggi/100)**2
+print(f"Berat Badan yang ideal untuk mencapai BMI yang diinginkan adalah: {BeratBadan:.2f}Kg")
