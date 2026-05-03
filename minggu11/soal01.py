a12dfcfb90f724cd12baf85fe77c56a9713cdb2e
Jason-prat <matthew.jason@ti.ukdw.ac.id>
2026-05-03 16:27:58 +0700

    m11

---
+++ minggu11/soal01.py
@@ -0,0 +1,13 @@
+angka=input("Masukkan angka(beri spasi antar angka): ").split()
+data=[]
+for i in angka:
+    try:
+        data.append(int(i))
+    except ValueError:
+        print(f"{i}, bukan angka")
+if len(data)<3:
+    print("Jumlah data ne Kurang")
+else:
+    angka=sorted(data)
+    angka.reverse()
+    print(f"3 Bilangan Terbaik: {angka[0]}, {angka[1]}, {angka[2]}")
